20e57cb5349ba4a241a9c6749495566a1892d7ae
chirag <chiragpaliwal147@gmail.com>
2023-07-21 00:16:18 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import HomePage from './Components/HomePage';
+import Container from './Components/Container';
+import CardContainer from './Components/CardContainer';
+import Banner from './Components/Banner';
+import Instruction from './Components/Instruction';
+import Company from './Components/Company';
+import GetInTouch from './Components/GetInTouch';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomePage />
+      <Container />
+      <CardContainer />
+      <Banner />
+      <Instruction />
+      <Company />
+      <GetInTouch />
+      <Footer />
     </div>
   );
 }
